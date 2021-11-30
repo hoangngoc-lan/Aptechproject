eee325af08e36579781055151efe1380b256023c
hoangngoc-lan <90963233+hoangngoc-lan@users.noreply.github.com>
2021-11-30 11:37:16 +0700

    update layout

---
+++ resources/views/bakery/layout/header.blade.php
@@ -0,0 +1,145 @@
+ <header class="top">
+                  <div class="fixedArea">
+          <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12 noPadding">
+                            <div class="content-wrapper one">
+                                <!-- Main Menu Start -->
+                                <!-- Navbar-->
+                                <header class="header">
+                                    <nav class="navbar navbar-default myNavBar">
+                                        <div class="container">
+
+                                            <!-- Brand and toggle get grouped for better mobile display -->
+                                            <div class="navbar-header">
+                                                <div class="row">
+                                                    <div class="col-md-9 col-sm-9 col-xs-9">
+                                                        <div class="row">
+                                                            <div class="col-md-3 col-xs-3 col-sm-3">
+                                                                <a style="padding-top:0px;" class="navbar-brand navBrandText text-uppercase font-weight-bold" href="index.html"><img src="/bakery/img/logo/cemrebakerylogo.png" alt="restorant" /></a>
+                                                                
+                                                            </div>
+                                                            <div class="col-md-9 col-sm-9 col-xs-9">
+                                                                <a href="index.html"><img class="img-responsive logo" src="/bakery/img/logo/cemrebakerytextlogolight.png" alt="restorant" /></a>
+                                                                
+                                                            </div>
+                                                        </div>
+
+
+                                                    </div>
+                                                    <div class="col-md-3 col-sm-3 col-xs-3">
+                                                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                                            <span class="sr-only">Toggle navigation</span>
+                                                            <span class="icon-bar"></span>
+                                                            <span class="icon-bar"></span>
+                                                            <span class="icon-bar"></span>
+                                                        </button>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+                                                     
+                                            <!-- Collect the nav links, forms, and other content for toggling -->
+                                            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                                            <ul class="nav navbar-nav navbar-right navBar">
+                                                    <li class="nav-item"><a href="#section0" class="nav-link text-uppercase font-weight-bold js-scroll-trigger">Home <span class="sr-only">(current)</span></a></li>
+                                                    <li class="nav-item"><a href="#section1" class="nav-link text-uppercase font-weight-bold js-scroll-trigger">Offers</a></li>
+                                                    <li class="nav-item"><a href="#section2" class="nav-link text-uppercase font-weight-bold js-scroll-trigger">About</a></li>
+                                                    <li class="nav-item"><a href="#section3" class="nav-link text-uppercase font-weight-bold js-scroll-trigger">Portfolio</a></li>
+                                                    <li class="nav-item"><a href="#section4" class="nav-link text-uppercase font-weight-bold js-scroll-trigger">Testimonial</a></li>
+                                                    <li class="nav-item"><a href="#section5" class="nav-link text-uppercase font-weight-bold js-scroll-trigger">Blog</a></li>
+                                                    <li class="nav-item"><a href="#section6" class="nav-link text-uppercase font-weight-bold js-scroll-trigger">Contact</a></li>
+                                                    <li class="nav-item"><a href="#section7" class="nav-link text-uppercase font-weight-bold js-scroll-trigger">Address</a></li>
+
+
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </nav>
+                                </header>
+                                <!-- Main Menu End -->
+                            </div>
+            </div>
+                  </div>
+      </div>
+    </header>
+    <!-- Header Area End -->
+    <!-- Section0 Area Start -->
+        <section id="section0" class="slider-area"> 
+            <div class="main-slider owl-theme owl-carousel"> 
+                <!-- Start Slingle Slide -->
+                <div class="single-slide item" style="background-image: url(/bakery/img/slider/1900x1000.png)">
+
+
+                    <!-- <img src="img/slider/placehold.png"> -->
+
+                    <!-- Start Slider Content -->
+
+                        <div class="slider-content-area">  
+                                <div class="row">
+                                        <div class="slide-content-wrapper text-center">
+                                            <div class="slide-content">
+                                                <img class="classic" src="/bakery/img/logo/cemrebakerylogo.png">
+                                                <h3>BAKERY MAKES </h3>
+                                                <h2>A Taste Of  The Good Life</h2>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis leo vitae lacinia congue.</p>
+
+                                                <a class="default-btn" href="about.html">Learn more</a>
+                                                <img class="classic" src="/bakery/img/new/icon.png">
+
+                                            </div>
+                                        </div>
+                                    </div>
+                    </div>
+                    <!-- Start Slider Content -->
+                </div>
+                <!-- End Single Slide -->
+                <!-- Start Slingle Slide -->
+                <div class="single-slide item" style="background-image: url(/bakery/img/slider/1900x1000.png)">
+
+                    <!-- <img src="img/slider/bakery.jpg"> -->
+                    <!-- Start Slider Content -->
+
+                        <div class="slider-content-area">   
+                                <div class="row">
+                                        <div class="slide-content-wrapper text-center">
+                                            <div class="slide-content">
+                                                <img class="classic" src="/bakery/img/logo/cemrebakerylogo.png">
+
+                                                <h3>BAKERY MAKES </h3>
+                                                <h2>A Taste Of  The Good Life</h2>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis leo vitae lacinia congue.</p>
+                                                <a class="default-btn" href="about.html">Learn more</a>
+                                                <img class="classic" src="/bakery/img/new/icon.png">
+
+                                            </div>
+                                    </div>
+                            </div>
+                  </div>
+                    <!-- Start Slider Content -->
+                </div>  
+                
+                <div class="single-slide item" style="background-image: url(/bakery/img/slider/1900x1000.png)">
+
+                    <!--<img src="img/slider/bakery.jpg">-->
+                    <!-- Start Slider Content -->
+
+                        <div class="slider-content-area">   
+                                <div class="row">
+                                        <div class="slide-content-wrapper text-center">
+                                            <div class="slide-content">
+                                                <img class="classic" src="/bakery/img/logo/cemrebakerylogo.png">
+
+                                                <h3>BAKERY MAKES </h3>
+                                                <h2>A Taste Of  The Good Life</h2>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis leo vitae lacinia congue.</p>
+                                                <a class="default-btn" href="about.html">Learn more</a>
+                                                <img class="classic" src="/bakery/img/new/icon.png">
+
+                                            </div>
+                                    </div>
+                            </div>
+                        </div>
+                    <!-- Start Slider Content -->
+                </div>
+             </div>
+        </section>
