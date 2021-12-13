a62e4f969f0039c3a5f16bff7ec7315e19b83161
thong0406 <55343500+thong0406@users.noreply.github.com>
2021-12-13 20:27:36 +0700

    Add files via upload

---
+++ resources/views/loginform/layout/style.blade.php
@@ -0,0 +1,2 @@
+<link rel="stylesheet" href="{{ asset('login_template/fonts/material-icon/css/material-design-iconic-font.min.css') }}">
+<link rel="stylesheet" href="{{ asset('login_template/css/style.css') }}">
