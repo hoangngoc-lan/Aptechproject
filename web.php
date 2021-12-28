e3e1b277910c00ec847090604b372c2e622b0ca6
hoangngoc-lan <90963233+hoangngoc-lan@users.noreply.github.com>
2021-12-28 13:13:56 +0700

    Add files via upload

---
+++ web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\bakery\HomeController;
+use App\Http\Controllers\login\LoginController;
+use App\Http\Controllers\cart\cart_controller;
+use App\Http\Controllers\bakery\DetailsController;
+
+Route::prefix('cart')->group(function () {
+	Route::get('/',[cart_controller::class,'cart'])->name('cart');
+	Route::post('/update/{id}',[cart_controller::class,'cart_update'])->name('cart_update');
+	Route::post('/order',[cart_controller::class,'cart_order'])->name('cart_order');
+	Route::get('delete_cart/{id}', [cart_controller::class, 'cart_delete'])->name('delete_cart');
+});
+
+Route::get('/', [HomeController::class, 'home'])->name('home');
+Route::get('ad', [HomeController::class, 'admin']);
+Route::get('signin', [HomeController::class, 'signin'])->name('login');
+Route::get('signup', [LoginController::class, 'signup'])->name('register');
+
+Route::get('details/{id}', [DetailsController::class, 'details'])->name('bakery.Details.bookdetail');
+Route::post('add_cart/{id}', [DetailsController::class, 'add_cart'])->name('add_cart');
